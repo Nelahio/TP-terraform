0225a04dbe6221f37042fb3032a1722290db6263
Nelahio <57316380+Nelahio@users.noreply.github.com>
2023-03-22 15:05:16 +0100

    création environnement de prod

--- main.tf
+++ main.tf
@@ -26,7 +26,8 @@ resource "azurerm_service_plan" "app-plan" {
 }
 
 resource "azurerm_linux_web_app" "webapp" {
-  name                = "web-${var.project_name}"
+  count               = var.environment_suffix == "-dev" ? 1 : 0
+  name                = "web-${var.project_name}${var.environment_suffix}"
   resource_group_name = data.azurerm_resource_group.rg-maalsi.name
   location            = data.azurerm_resource_group.rg-maalsi.location
   service_plan_id     = azurerm_service_plan.app-plan.id
