976d857d5e4b9692adf4bd90d5131c68af8a3c66
Nelahio <57316380+Nelahio@users.noreply.github.com>
2023-03-22 11:49:19 +0100

    outputs et variables

---
+++ variables.tf
@@ -0,0 +1,14 @@
+variable "environment_suffix" {
+  type = string
+  description = "Procure le suffixe indiquant l'environnement cible"
+}
+
+variable "location" {
+  type = string
+  default = "West Europe"
+}
+
+variable "project_name" {
+  type = string
+  default = "adislaire"
+}
