2f7593345734ccb1326fb219cbf632c39e8d52a5
Nelahio <57316380+Nelahio@users.noreply.github.com>
2023-03-23 09:42:13 +0100

    tags inutiles

--- main.tf
+++ main.tf
@@ -35,10 +35,6 @@ resource "azurerm_mssql_database" "sql-db" {
   read_scale     = false
   sku_name       = "S0"
   zone_redundant = false
-
-  tags = {
-    foo = "bar"
-  }
 }
 
 resource "azurerm_service_plan" "app-plan" {
@@ -56,7 +52,7 @@ resource "azurerm_linux_web_app" "web-app" {
   service_plan_id     = azurerm_service_plan.app-plan.id
 
   site_config {}
-  
+
   connection_string {
     name = "DefaultConnection"
     value = "Server=tcp:${azurerm_mssql_server.sql-srv.fully_qualified_domain_name},1433;Initial Catalog=${azurerm_mssql_database.sql-db.name};Persist Security Info=False;User ID=${data.azurerm_key_vault_secret.database-login.value};Password=${data.azurerm_key_vault_secret.database-password.value};MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;"
