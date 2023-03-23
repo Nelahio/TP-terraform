4b1c7bb360633fa61b0360a0b2b7f155531d870f
Nelahio <57316380+Nelahio@users.noreply.github.com>
2023-03-23 10:33:15 +0100

    adding rabbitmq container

--- main.tf
+++ main.tf
@@ -26,6 +26,7 @@ resource "azurerm_mssql_server" "sql-srv" {
   administrator_login_password = data.azurerm_key_vault_secret.database-password.value
 }
 
+# Database
 resource "azurerm_mssql_database" "sql-db" {
   name           = "RabbitMqDemo"
   server_id      = azurerm_mssql_server.sql-srv.id
@@ -37,6 +38,7 @@ resource "azurerm_mssql_database" "sql-db" {
   zone_redundant = false
 }
 
+#Web App
 resource "azurerm_service_plan" "app-plan" {
   name                = "plan-${var.project_name}${var.environment_suffix}"
   resource_group_name = data.azurerm_resource_group.rg-maalsi.name
@@ -60,8 +62,40 @@ resource "azurerm_linux_web_app" "web-app" {
   }
 
   app_settings = {
-    "RabbitMQ__Hostname" = "rabbitmq",
+    "RabbitMQ__Hostname" = azurerm_container_group.rabbitmq.fqdn,
     "RabbitMQ__Username" = data.azurerm_key_vault_secret.rabbitmq-login.value,
     "RabbitMQ__Password" = data.azurerm_key_vault_secret.rabbitmq-password.value
+  }
+}
+
+#RabbitMQ Container Instance
+resource "azurerm_container_group" "rabbitmq" {
+  name                = "aci-mq-${var.project_name}${var.environment_suffix}"
+  resource_group_name = data.azurerm_resource_group.rg-maalsi.name
+  location            = data.azurerm_resource_group.rg-maalsi.location
+  ip_address_type     = "Public"
+  dns_name_label      = "aci-mq-${var.project_name}${var.environment_suffix}"
+  os_type             = "Linux"
+
+  container {
+    name   = "rabbitmq"
+    image  = "rabbitmq:3-management"
+    cpu    = "0.5"
+    memory = "1.5"
+
+    ports {
+      port     = 5672
+      protocol = "TCP"
+    }
+
+    ports {
+      port     = 15672
+      protocol = "TCP"
+    }
+
+    environment_variables = {
+      "RABBITMQ_DEFAULT_USER" = data.azurerm_key_vault_secret.rabbitmq-login.value,
+      "RABBITMQ_DEFAULT_PASS" = data.azurerm_key_vault_secret.rabbitmq-password.value
+    }
   }
 }
